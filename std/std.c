03ac2ba800d47b81b7419fa7893e4452c5946e1a
nikos <floaters@runbox.com>
2020-04-08 16:06:32 +0300

    elementary I/O added

--- std/std.c
+++ std/std.c
@@ -4,19 +4,24 @@
 
 struct student 
 {
-    char first[32];
-    char last[32];
+    char first[16];
+    char last[16];
     int roll;
     float mark;
 };
-
-struct student st[SIZE];
 
 void initarray(struct student *array)
 {
     for (int i = 0; i < SIZE; i++)
     {
+        for (int j = 0; j < 15; j++)
+        {
+            array[i].first[j] = '-';
+            array[i].last[j] = '-';
+        }
+        
         array[i].roll = 0;
+        array[i].mark = 0;
     }
 }
 
@@ -76,11 +81,154 @@ void printarray(struct student *array)
     printf("\n");
 }
 
+int file_detected(char *filename)
+{
+    FILE *fptr;
+    fptr = fopen(filename, "r");
+    
+    if(fptr == NULL)
+    {  
+        return 0;             
+    }
+
+    fclose(fptr);
+    return 1;
+}
+
 int main()
 {
+    struct student st[SIZE];
+    struct student rc[SIZE];
+    
+    char ans = 'z';
+    char buffer[128];
 
-    fillarray(st);
-    printarray(st);
+    int i;
+
+    FILE *fp;
+
+    initarray(st);
+    initarray(rc);
+
+    if (file_detected("record.txt"))
+    {
+        printf("\n");
+        printf("\n");
+        printf("\t >>>> FILE RECORD DETECTED\n");
+        printf("\n");
+        
+        while (ans != 'r' && ans != 'd' && ans != 'w' && ans != 'q')
+        {
+            initarray(st);
+            initarray(rc);
+            
+            printf("\n");
+            printf("[r] \t FILE READ\n");
+            printf("[d] \t FILE DELETE\n");
+            printf("[w] \t FILE WRITE\n");
+            printf("[q] \t QUIT\n");
+            printf("\n");
+            printf("> ");
+            scanf("%s", &ans);
+
+            if (ans != 'r' && ans != 'd' && ans != 'w' && ans != 'q')
+            {
+                printf("OPTION [%s] DOES NOT EXIST (YET)\n", &ans);
+            }
+
+            if (ans == 'q')
+            {
+                return 0;
+            }
+
+            if (ans == 'r')
+            {
+                fp = fopen("record.txt", "r");
+
+                while (fgets(buffer, 144, fp) != NULL)
+                {
+                    printf("%s\n", buffer);
+                }
+
+                fclose(fp);
+                ans = 'e';
+            }
+
+            if (ans == 'd')
+            {
+                fp = fopen("record.txt", "w");
+            
+                for (i = 0; i < SIZE; i++)
+                {
+                    fprintf(fp, "%s %s %d %f\n", rc[i].first, rc[i].last, rc[i].roll, rc[i].mark);
+                }
+
+                fclose(fp);
+                ans = 'e';
+            }
+
+            if (ans == 'w')
+            {
+                fillarray(st);
+                printarray(st);
+
+                printf("\n");
+
+                while (ans != 'y' && ans != 'n')
+                {
+                    printf("STORE ARRAY IN FILE (y/n): ");
+                    scanf("%s", &ans);
+                    printf("\n");
+                }
+
+                if (ans == 'y')
+                {
+                    fp = fopen("record.txt", "w");
+            
+                    for (i = 0; i < SIZE; i++)
+                    {
+                        fprintf(fp, "%s %s %d %f\n", st[i].first, st[i].last, st[i].roll, st[i].mark);
+                    }
+
+                    fclose(fp);
+                    ans = 'e';
+                }
+            }
+        } 
+    }
+    else
+    {
+        printf("\n");
+        printf("\n");
+        printf("\t >>>> NO FILE RECORD DETECTED\n");
+        printf("\n");
+
+        fillarray(st);
+        printarray(st);
+
+        printf("\n");
+
+        while (ans != 'y' && ans != 'n')
+        {
+            printf("STORE ARRAY IN FILE (y/n): ");
+            scanf("%s", &ans);
+            printf("\n");
+        }
+
+        if (ans == 'y')
+        {
+            fp = fopen("record.txt", "w");
+            
+            for (i = 0; i < SIZE; i++)
+            {
+                fprintf(fp, "%s %s %d %f\n", st[i].first, st[i].last, st[i].roll, st[i].mark);
+            }
+
+            fclose(fp);
+
+        }
+        
+    }
     return 0;
 }
 
