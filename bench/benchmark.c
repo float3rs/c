d65cdc653f021357e06a9f1f89936f138c9b8845
nikos <floaters@runbox.com>
2020-06-04 09:33:53 +0300

    before refactoring

--- bench/benchmark.c
+++ bench/benchmark.c
@@ -14,40 +14,56 @@
 // #define SIZE SHRT_MAX
 #define SIZE 1000
 
+
 void syntax_print()
 {
-    printf("\n|--------------------------------|\n");
-    printf(  "| INVALID CALL ARGUEMENTS        |\n");
-    printf(  "| PROPER BENCHMARK SYNTAX:       |\n");
-    printf(  "|--------------------------------|\n");
-    printf(  "| ./benchmark nxy [--debug]      |\n");           
-    printf(  "|--------------------------------|\n");
-    printf(  "|  size |------------------------|\n");
-    printf(  "|-------|------------------------|\n");
-    printf(  "| n = 0 |   3276 (SHT_MAX / 10)  |\n");
-    printf(  "| n = 1 |  32767 (SHT_MAX     )  |\n");
-    printf(  "| n = 2 | 327670 (SHT_MAX * 10)  |\n");
-    printf(  "|-------|------------------------|\n");
-    printf(  "| range |----------------------- |\n");
-    printf(  "|-------|------------------------|\n");
-    printf(  "| x = 0 | unique many            |\n");
-    printf(  "| x = 1 | few & large            |\n");
-    printf(  "| x = 2 | few & small            |\n");
-    printf(  "|-------|------------------------|\n");
-    printf(  "|  sort |------------------------|\n");
-    printf(  "|-------|------------------------|\n");
-    printf(  "| y = 0 |      unsorted          |\n");
-    printf(  "| y = 1 |        sorted          |\n");
-    printf(  "| y = 2 | mostly sorted          |\n");
-    printf(  "| y = 3 |        sorted reversed |\n");
-    printf(  "| y = 4 | mostly sorted reversed |\n");
-    printf(  "|--------------------------------|\n");
-    printf(  "|       --debug: print arrays    |\n");
-    printf(  "|--------------------------------|\n\n");
+    printf("\n|--------------------------------  |\n");
+    printf(  "| INVALID CALL ARGUEMENTS          |\n");
+    printf(  "| PROPER BENCHMARK SYNTAX:         |\n");
+    printf(  "|--------------------------------  |\n");
+    printf(  "| ./benchmark nxyz [-xX] [--debug] |\n");           
+    printf(  "|----------------------------------|\n");
+    printf(  "|  size |--------------------------|\n");
+    printf(  "|-------|--------------------------|\n");
+    printf(  "| n = 0 |   3276 (SHT_MAX / 10)    |\n");
+    printf(  "| n = 1 |  32767 (SHT_MAX     )    |\n");
+    printf(  "| n = 2 | 327670 (SHT_MAX * 10)    |\n");
+    printf(  "|-------|--------------------------|\n");
+    printf(  "| range |--------------------------|\n");
+    printf(  "|-------|--------------------------|\n");
+    printf(  "| x = 0 | unique many              |\n");
+    printf(  "| x = 1 | few & large              |\n");
+    printf(  "| x = 2 | few & small              |\n");
+    printf(  "|-------|--------------------------|\n");
+    printf(  "| order |--------------------------|\n");
+    printf(  "|-------|--------------------------|\n");
+    printf(  "| y = 0 |      unsorted            |\n");
+    printf(  "| y = 1 |        sorted            |\n");
+    printf(  "| y = 2 | mostly sorted            |\n");
+    printf(  "| y = 3 |        sorted reversed   |\n");
+    printf(  "| y = 4 | mostly sorted reversed   |\n");
+    printf(  "|-------|--------------------------|\n");
+    printf(  "|  sort |--------------------------|\n");
+    printf(  "|-------|--------------------------|\n");
+    printf(  "| z = 0 |                   none   |\n");
+    printf(  "| z = 1 |         selection sort   |\n");
+    printf(  "| z = 2 |         insertion sort   |\n");
+    printf(  "| z = 3 |            bubble sort   |\n");
+    printf(  "| z = 4 |              quicksort   |\n");
+    printf(  "|-------|--------------------------|\n");
+    printf(  "| timer |--------------------------|\n");
+    printf(  "|-------|--------------------------|\n");
+    printf(  "|   -m3 |       mean time / 3 runs |\n");
+    printf(  "|   -c9 | cumulative time / 9 runs |\n");
+    printf(  "|----------------------------------|\n");
+    printf(  "|       --debug: print arrays      |\n");
+    printf(  "|----------------------------------|\n\n");
 }
 
 void array_print(long* r, int size)
 {
+    printf("\n");
+    
     for (int i = 0; i < size; i++)
     {
         printf("{%5d} %ld\n", i, r[i]);
@@ -168,20 +184,149 @@ void mostly_unsort(long* r, int size, int bucket_size)
 
 int main(int argc, char *argv[])
 {   
-    int i, size;
+    int i; 
+    int size;
+    
+    int repeats = 1;
+    int result = 0;
+    
+    int mean = 0;
+    int cumulative = 0;
+    int debug = 0;
+
     long* r;
 
-    if ((argc < 2) || (argc > 3))
+    time_t start[9]; 
+    time_t end[9];
+
+    if ((argc < 2) || (argc > 4))
     {
         syntax_print();
         return 1;
     }
+    else
+    {
+        switch (argc)
+        {
+            case 2:
 
+                mean = 0;
+                cumulative = 0;
+                repeats = 1;
+                debug = 0;
+                
+                break;
+            
+            case 3:
+
+                if (!strcmp(argv[2], "--debug"))
+                {
+                    debug = 1;
+                    // array_print(r, size);
+                }
+                else
+                {
+                    if ((argv[2][0] == '-') && (argv[2][1] == 'm'))
+                    {
+                        mean = 1;
+                        cumulative = 0;
+
+                        if (atoi(&argv[2][2]))
+                        {
+                            repeats = atoi(&argv[2][2]);
+                        }
+                        else
+                        {
+                            repeats = 1;
+                        }
+                           
+                    }
+                    else
+                    {
+                        if ((argv[2][0] == '-') && (argv[2][1] == 'c'))
+                        {
+                            mean = 0;
+                            cumulative = 1;
+                            
+                            if (atoi(&argv[2][2]))
+                            {
+                                repeats = atoi(&argv[2][2]);
+                            }
+                            else
+                            {
+                                repeats = 1;
+                            }
+                        }
+                        else
+                        {
+                            syntax_print();
+                            return 1;
+                        }
+                    }
+                }
+
+                break;
+            
+            case 4:
+
+                if ((argv[2][0] == '-') && (argv[2][1] == 'm'))
+                {
+                    mean = 1;
+                    cumulative = 0;
+
+                    if (atoi(&argv[2][2]))
+                    {
+                        repeats = atoi(&argv[2][2]);
+                    }
+                    else
+                    {
+                        repeats = 1;
+                    }
+                        
+                }
+                else
+                {
+                    if ((argv[2][0] == '-') && (argv[2][1] == 'c'))
+                    {
+                        mean = 1;
+                        cumulative = 0;
+                        
+                        if (atoi(&argv[2][2]))
+                        {
+                            repeats = atoi(&argv[2][2]);
+                        }
+                        else
+                        {
+                            repeats = 1;
+                        }
+                    }
+                    else
+                    {
+                        syntax_print();
+                        return 1;
+                    }
+                }
+
+                if (!strcmp(argv[3], "--debug"))
+                {
+                    debug = 1;
+                    // array_print(r, size);
+                }
+                else
+                {
+                    syntax_print();
+                    return 1;
+                }
+                
+                break;
+        }
+    }
+    
     switch (argv[1][0])
     {
         case '0':
 
-            printf("> size: %d keys\n", SIZE / 10);
+            printf("\n>   size: %d keys\n", SIZE / 10);
 
             size = SIZE / 10;
             r = (long*)(malloc(size * sizeof(long)));
@@ -190,7 +335,7 @@ int main(int argc, char *argv[])
             {
                 case '0':
 
-                    printf("> range: [0 - %d]\n", MANY);
+                    printf(">  range: [0 - %d]\n", MANY);
 
                     fill_keys(r, size, MANY, 0);
                     // array_print(r, size);
@@ -199,7 +344,7 @@ int main(int argc, char *argv[])
 
                 case '1':
 
-                    printf("> range: [%d - %d]\n", MANY-FEW, MANY);
+                    printf(">  range: [%d - %d]\n", MANY-FEW, MANY);
 
                     fill_keys(r, size, FEW, MANY-FEW);
                     // array_print(r, size);
@@ -208,7 +353,7 @@ int main(int argc, char *argv[])
 
                 case '2':
 
-                    printf("> keys: [0 - %d]\n", FEW);
+                    printf(">  range: [0 - %d]\n", FEW);
 
                     fill_keys(r, size, FEW, 0);
                     // array_print(r, size);
@@ -225,7 +370,7 @@ int main(int argc, char *argv[])
 
         case '1':
 
-            printf("> size: %d keys\n", SIZE);
+            printf("\n>   size: %d keys\n", SIZE);
 
             size = SIZE;
             r = (long*)(malloc(size * sizeof(long)));
@@ -234,7 +379,7 @@ int main(int argc, char *argv[])
             {
                 case '0':
 
-                    printf("> range: [0 - %d]\n", MANY);
+                    printf(">  range: [0 - %d]\n", MANY);
 
                     fill_keys(r, size, FEW, 0);
                     // array_print(r, size);
@@ -243,7 +388,7 @@ int main(int argc, char *argv[])
 
                 case '1':
 
-                    printf("> keys: [%d - %d]\n", MANY-FEW, MANY);
+                    printf(">  range: [%d - %d]\n", MANY-FEW, MANY);
 
                     fill_keys(r, size, FEW, MANY-FEW);
                     // array_print(r, size);
@@ -252,7 +397,7 @@ int main(int argc, char *argv[])
 
                 case '2':
 
-                    printf("> range: [0 - %d]\n", FEW);
+                    printf(">  range: [0 - %d]\n", FEW);
 
                     fill_keys(r, size, FEW, 0);
                     // array_print(r, size);
@@ -269,7 +414,7 @@ int main(int argc, char *argv[])
 
         case '2':
 
-            printf("> size: %d keys\n", SIZE * 10);
+            printf("\n>   size: %d keys\n", SIZE * 10);
             
             size = SIZE * 10;
             r = (long*)(malloc(size * sizeof(long)));
@@ -278,7 +423,7 @@ int main(int argc, char *argv[])
             {
                 case '0':
 
-                    printf("> range: [0 - %d]\n", MANY);
+                    printf(">  range: [0 - %d]\n", MANY);
 
                     fill_keys(r, size, MANY, 0);
                     // array_print(r, size);
@@ -287,7 +432,7 @@ int main(int argc, char *argv[])
 
                 case '1':
 
-                    printf("> range: [%d - %d]\n", MANY-FEW, MANY);
+                    printf(">  range: [%d - %d]\n", MANY-FEW, MANY);
 
                     fill_keys(r, size, FEW, MANY-FEW);
                     // array_print(r, size);
@@ -296,7 +441,7 @@ int main(int argc, char *argv[])
 
                 case '2':
 
-                    printf("> range: [0 - %d]\n", FEW);
+                    printf(">  range: [0 - %d]\n", FEW);
 
                     fill_keys(r, size, FEW, 0);
                     // array_print(r, size);
@@ -321,13 +466,13 @@ int main(int argc, char *argv[])
     {
         case '0':
 
-            printf("> sort: unsorted\n");
+            printf(">  order: unsorted\n");
 
             break;
 
         case '1':
 
-            printf("> sort: sorted\n");
+            printf(">  order: sorted\n");
 
             qsort(r, size, sizeof(long), intcmp_asc);
             // array_print(r, size);
@@ -336,7 +481,7 @@ int main(int argc, char *argv[])
 
         case '2':
 
-            printf("> sort: mostly sorted\n");
+            printf(">  order: mostly sorted\n");
 
             qsort(r, size, sizeof(long), intcmp_asc);
             // array_print(r, size);
@@ -347,7 +492,7 @@ int main(int argc, char *argv[])
 
         case '3':
 
-            printf("> sort: sorted reversed\n");
+            printf(">  order: sorted reversed\n");
 
             qsort(r, size, sizeof(long), intcmp_des);
             // array_print(r, size);
@@ -356,7 +501,7 @@ int main(int argc, char *argv[])
 
         case '4':
 
-            printf("> sort: mostly sorted reversed\n");
+            printf(">  order: mostly sorted reversed\n");
 
             qsort(r, size, sizeof(long), intcmp_des);
             // array_print(r, size);
@@ -371,18 +516,142 @@ int main(int argc, char *argv[])
             return 1;
     }
 
-    if (argc == 3)
-    { 
-        if (!strcmp(argv[2], "--debug"))
+    switch (argv[1][3])
+    {
+        case '0':
+
+            printf(">   sort: -\n");
+
+            start[0] = -1;
+            end[0] = -1;
+
+            break;
+
+        case '1':
+
+            printf(">   sort: selection\n");
+
+            
+            // array_print(r, size);
+
+            break;
+
+        case '2':
+
+            printf(">   sort: insertion\n");
+
+            
+            // array_print(r, size);
+
+            
+
+            break;
+
+        case '3':
+
+            printf(">   sort: bubble\n");
+
+            
+            // array_print(r, size);
+
+            break;
+
+        case '4':
+
+            printf(">   sort: quicksort\n");
+
+            
+
+            break;
+
+        case '5':
+
+            printf(">   sort: qsort()\n");
+
+            for (i = 0; i < repeats; i++)
+            {
+                start[i] = clock();
+                qsort(r, size, sizeof(long), intcmp_asc);
+                end[i] = clock();
+            }
+
+            break;
+        
+        default:
+
+            syntax_print();
+            return 1;
+    }
+
+    if ((start[0] < 0) && (end[0] < 0))
+    {
+        printf(">   time: -\n");
+        printf("> result: -\n");
+    }
+    else
+    {
+        if (mean)
         {
-            array_print(r, size);
+            printf(">   time: mean (%d runs)\n", repeats);
+
+            result = 0;
+
+            for (i = 0; i < repeats; i++)
+            {
+                result += (end[i] - start[i]);
+            }
+            
+            result /= repeats;
+
+            printf("> result: %f \n", (double) result / CLOCKS_PER_SEC);
         }
         else
         {
-            syntax_print();
-            return 1;
+            if (cumulative)
+            {
+                printf(">   time: cumulative (%d runs)\n", repeats);
+
+                result = 0;
+
+                for (i = 0; i < repeats; i++)
+                {
+                    result += (end[i] - start[i]);
+                }
+
+                printf("> result: %f seconds\n", (double) result / CLOCKS_PER_SEC);
+            }
+            else
+            {
+                result = end[0] - start[0];
+                
+                printf(">   time: direct measurement\n");
+                printf("> result: %f \n", (double) result / CLOCKS_PER_SEC);
+            }
+            
         }
+        
+        
     }
+
+    printf("\n");
+    
+    if (debug)
+    {
+        array_print(r, size);
+        
+        printf("\n      mean ? %d\n", mean);
+        printf(  "cumulative ? %d\n", cumulative);
+        printf(  "   repeats = %d\n\n", repeats);
+
+        for (i = 0; i < repeats; i++)
+        {
+            printf("end[%d]: %ld - start[%d]: %ld = %ld\n", i, end[i], i, start[i], (end[i] - start[i]));
+        }
+
+        printf("\n");
+    }
+    
+    
 
     return 0;
 }
